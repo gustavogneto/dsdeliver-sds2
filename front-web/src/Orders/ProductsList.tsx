3649b969352f03e015464225604d1af77f3c51d7
Gustavo <gustavo.g.neto@gmail.com>
2021-01-21 16:01:11 -0300

    Criada logica de seleção e totalizadores

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,17 +1,25 @@
 import { type } from "os";
+import { checkIsSelected } from "./Helpers";
 import ProductCard from "./ProductCard";
 import { Product } from "./types";
 
 type Props = {
     product: Product[];
+    onSelectProduct: (product: Product) => void;
+    selectedProducts: Product[];
 }
 
-function ProductsList({ product }: Props) {
+function ProductsList({ product,selectedProducts, onSelectProduct }: Props) {
     return (
         <div className="orders-list-container">
             <div className="orders-list-items">
             {product.map(product =>(
-                <ProductCard key={product.id} product={product} />
+                <ProductCard 
+                    key={product.id} 
+                    product={product} 
+                    onSelectProduct={onSelectProduct}
+                    isSelected={checkIsSelected(selectedProducts, product)}
+                    />
             ))}
              
             </div>
