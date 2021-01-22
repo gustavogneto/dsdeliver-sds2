2f98e02b1101fcad469adeccd5f007f064b96ac9
gustavogneto <32483792+gustavogneto@users.noreply.github.com>
2021-01-22 12:16:16 -0300

    correção das variáveis de ambiente
    correção das variáveis de ambiente

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { OrderPayLoad } from "./Orders/types";
 
-const API_URL = 'http://localhost:8080'
+const API_URL = process.env.REACT_APP_API_URL
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX
-//const API_URL = 'https://delivernew.herokuapp.com/'
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
