da97d1e6e9b260eb60d9b73aaa5d8f40105277ee
Gustavo <gustavo.g.neto@gmail.com>
2021-01-27 16:39:12 -0300

    correcao para deploy 1

--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -9,9 +9,6 @@ export function fetchOrders(){
     return axios(`${API_URL}/orders`);
 }
 
-export function fetchLocalMapBox(local:string ){
-    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
-}
 
 
 export function confirmDelivery(orderId: number){
