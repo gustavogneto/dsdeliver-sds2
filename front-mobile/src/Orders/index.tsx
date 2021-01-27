4625f019853419c15c3a0f8a7d6099af8fb6e922
Gustavo <gustavo.g.neto@gmail.com>
2021-01-27 09:39:55 -0300

    add list orders front mobile

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Header from '../Header';
-
+import OrderCard from '../OrderCard';
 
 export default function Orders() {
 
@@ -11,13 +11,24 @@ export default function Orders() {
     return (
         <>
             <Header />
-            <View>
-                <Text> listagem de pedidos</Text>
-            </View>
+            <ScrollView style={styles.container}>
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+             <OrderCard />
+            </ScrollView>
         </>
     );
 }
 
 const styles = StyleSheet.create({
+    container:{
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    }
 
 });
